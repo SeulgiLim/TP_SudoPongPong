28345607bcd1ef8a98cc4afc298b9fbd3efafb86
SeulgiLim <71672106+SeulgiLim@users.noreply.github.com>
2021-05-24 22:41:36 +0900

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.go.mapo.unsplash_app_tutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
